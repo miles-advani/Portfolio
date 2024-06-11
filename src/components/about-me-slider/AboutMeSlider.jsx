40cfcbfa5536836446d44afa174ee6bc7659afce
Miles Advani <146325158+MilesAdvani@users.noreply.github.com>
2024-06-11 13:22:20 +0200

    Changed preview image for meta data

--- src/components/about-me-slider/AboutMeSlider.jsx
+++ src/components/about-me-slider/AboutMeSlider.jsx
@@ -1,10 +1,12 @@
 import "./AboutMeSlider.css";
 
-export default function AboutMeSlider({ header, content }) {
+export default function AboutMeSlider({ header, paragraphOne, paragraphTwo, paragraphThree}) {
   return (
     <div className="about-me-slider">
       <h2 className="about-me-slider-header">{header}</h2>
-      <p className="about-me-slider-content">{content}</p>
+      <p className="about-me-slider-content">{paragraphOne}</p>
+      <p className="about-me-slider-content">{paragraphTwo}</p>
+      <p className="about-me-slider-content">{paragraphThree}</p>
     </div>
   );
 }
