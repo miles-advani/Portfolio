3e3a636667c922148291b8e00991c6e494f2a269
Miles Advani <146325158+MilesAdvani@users.noreply.github.com>
2024-06-11 22:06:42 +0200

    Added hashlink for the logo link

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -1,11 +1,11 @@
-import { Link } from "react-router-dom";
+import { HashLink as Link } from "react-router-hash-link";
 import "./Logo.css";
 
 export default function Logo() {
     return (
       <ul className="logo">
         <li>
-        <Link to="/">ma<span>dev</span></Link>
+        <Link to="/#top">ma<span>dev</span></Link>
         </li>
       </ul>
     );
