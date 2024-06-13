4bc0940a3ffccce980b4659c7b396a37daf3f315
Miles Advani <146325158+MilesAdvani@users.noreply.github.com>
2024-06-13 14:10:05 +0200

    Added logo svg and favicon svg

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -1,12 +1,24 @@
 import { HashLink as Link } from "react-router-hash-link";
+import logo from '../../assets/logo.svg';
 import "./Logo.css";
 
+// export default function Logo() {
+//     return (
+//       <ul className="logo">
+//         <li>
+//         <Link to="/#top">ma<span>dev</span></Link>
+//         </li>
+//       </ul>
+//     );
+//   }
 export default function Logo() {
-    return (
-      <ul className="logo">
-        <li>
-        <Link to="/#top">ma<span>dev</span></Link>
-        </li>
-      </ul>
-    );
-  }
+  return (
+    <ul className="logo">
+      <li>
+        <Link to="/#top">
+          <img src={logo} alt="Logo" />
+        </Link>
+      </li>
+    </ul>
+  );
+}
