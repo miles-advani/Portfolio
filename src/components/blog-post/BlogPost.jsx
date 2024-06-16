db04909f9b310e6aa64f1e21404991112dd41c67
Miles Advani <146325158+MilesAdvani@users.noreply.github.com>
2024-06-16 18:03:24 +0200

    Added blog post content for terminal tool and tic tac toe

--- src/components/blog-post/BlogPost.jsx
+++ src/components/blog-post/BlogPost.jsx
@@ -12,14 +12,16 @@
 import "./BlogPost.css";
 
 export default function BlogPost({ header, content }) {
-  if (!header && !content) {
+  if (!header) {
     return null;
   }
 
   return (
     <div className="blog-post">
-      {header && <h2 className="blog-post-header">{header}</h2>}
-      {content && <p>{content}</p>}
+      <h2 className="blog-post-header">{header}</h2>
+      {content.map((paragraph, index) => (
+        <p key={index}>{paragraph}</p>
+      ))}
     </div>
   );
 }
