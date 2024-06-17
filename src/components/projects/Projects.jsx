de28305606f91e25a82660b81c97368a2ca28502
Miles Advani <146325158+MilesAdvani@users.noreply.github.com>
2024-06-17 19:54:12 +0200

    Added new header for projects section

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -13,25 +13,38 @@ export default function Projects() {
   return (
     <>
       <main id="projects">
-        <section className="projects-container">
+        {layoutData.projectHeaders.allProjects !== "" && (
           <h2 className="projects-header">
-            {layoutData.projectHeaders.myProjects}
+            {layoutData.projectHeaders.allProjects}
           </h2>
+        )}
 
-          {Object.keys(projectData).map((id) => (
-            <ProjectCard key={id} id={id} {...projectData[id]} />
-          ))}
-        </section>
-
-        <section className="collaborations-container">
-          <h2 className="projects-header">
-            {layoutData.projectHeaders.collaborations}
-          </h2>
-
-          {Object.keys(projectCollaborationsData).map((id) => (
-            <ProjectCard key={id} id={id} {...projectCollaborationsData[id]} />
-          ))}
-        </section>
+        <div className="projects-container">
+          <section className="my-projects-container">
+            {layoutData.projectHeaders.myProjects !== "" && (
+              <h2 className="projects-header">
+                {layoutData.projectHeaders.myProjects}
+              </h2>
+            )}
+            {Object.keys(projectData).map((id) => (
+              <ProjectCard key={id} id={id} {...projectData[id]} />
+            ))}
+          </section>
+          <section className="collaborations-container">
+            {layoutData.projectHeaders.collaborations !== "" && (
+              <h2 className="projects-header">
+                {layoutData.projectHeaders.collaborations}
+              </h2>
+            )}
+            {Object.keys(projectCollaborationsData).map((id) => (
+              <ProjectCard
+                key={id}
+                id={id}
+                {...projectCollaborationsData[id]}
+              />
+            ))}
+          </section>
+        </div>
       </main>
     </>
   );
